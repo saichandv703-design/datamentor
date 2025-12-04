9a5569507b1aeaebe8fccc3e994d074bc51c4997
saichandv703-design <saichandv703@gmail.com>
2025-12-03 19:41:29 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gfg: {
+          green: '#2F8D46',
+          'green-dark': '#0F7C34',
+          'green-light': '#3CAA54',
+          gray: '#f7f7f7',
+          border: '#e3e6e8',
+        },
+      },
+    },
   },
   plugins: [],
 };
